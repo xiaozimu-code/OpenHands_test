e4597832c3adeaa3058ae9592dd87ecc7c7e7ad9
xiaozimu-code <120485939@qq.com>
2025-07-21 17:08:15 +0800

    modify ip

--- openhands/agenthub/codeact_agent/codeact_agent.py
+++ openhands/agenthub/codeact_agent/codeact_agent.py
@@ -196,7 +196,7 @@ class CodeActAgent(Agent):
         params['extra_body'] = {'metadata': state.to_llm_metadata(agent_name=self.name)}
         # codeact在此处使用litellm发起模型的api调用  TODO 接一个转发服务，将参数传到容器外，转发服务连通容器和平台
         # response = self.llm.completion(**params)
-        response = resp = requests.post("http://127.0.0.1:18192/call_model_api",json=params)
+        response = resp = requests.post("http://10.79.208.49:18192/call_model_api",json=params)
         # logger.info(f'Response from LLM: {response}')  # 容器内不输出，加一条info级别的日志试试
         logger.debug(f'Response from LLM: {response}')
         actions = self.response_to_actions(response)
